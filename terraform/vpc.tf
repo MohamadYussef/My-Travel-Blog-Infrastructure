468019153ddff8f29d64aba460afe1fe31a3d4ee
MohamadQubaisi <mohammad.sherif.q@gmail.com>
2023-06-13 18:40:24 +0300

    modifyed network infra

---
+++ terraform/vpc.tf
@@ -0,0 +1,28 @@
+# Resource: aws_vpc
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+
+resource "aws_vpc" "main" {
+  # The CIDR block for the VPC.
+  cidr_block = "10.0.0.0/16"
+
+  # Makes your instances shared on the host.
+  instance_tenancy = "default"
+
+  # Required for EKS. Enable/disable DNS support in the VPC.
+  enable_dns_support = true
+
+  # Required for EKS. Enable/disable DNS hostnames in the VPC.
+  enable_dns_hostnames = true
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name = "main"
+  }
+}
+
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "VPC id."
+  # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
+  sensitive = false
+}
